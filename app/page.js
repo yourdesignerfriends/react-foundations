67175c28afad9e8985a92eccf8c478afaf466007
Analina Nielsen <analinanielsen@outlook.com>
2026-07-14 20:03:04 -0700

    Completed Chapter 10

--- app/page.js
+++ app/page.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import LikeButton from './like-button';
  
 function Header({ title }) {
   return <h1>{title ? title : 'Default title'}</h1>;
@@ -6,13 +6,7 @@ function Header({ title }) {
  
 export default function HomePage() {
   const names = ['Ada Lovelace', 'Grace Hopper', 'Margaret Hamilton'];
- 
-  const [likes, setLikes] = useState(0);
- 
-  function handleClick() {
-    setLikes(likes + 1);
-  }
- 
+
   return (
     <div>
       <Header title="Develop. Preview. Ship." />
@@ -22,7 +16,7 @@ export default function HomePage() {
         ))}
       </ul>
  
-      <button onClick={handleClick}>Like ({likes})</button>
+      <LikeButton />
     </div>
   );
 }
